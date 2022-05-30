ba10f95953d8d1e81b1bd58eb7fd2fb2b135d66e
kavin2309 <kavin2309k@gmail.com>
2022-05-30 19:57:54 +0530

    Done

---
+++ src/org/git/sample1.java
@@ -0,0 +1,20 @@
+package org.git;
+
+public class sample1 {
+	
+	public static void main(String[] args) {
+		 
+		System.out.println("JAVA");
+		
+		System.out.println(12345);
+		
+		System.out.println("PYHTON");
+		
+		System.out.println(678990);
+		
+		System.out.println("SQL");
+		
+		System.out.println(98765);
+	}
+
+}
